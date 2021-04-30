d492aafd9afceb0dbcfb4ac2c9929b6698907ec8
crappypapa <devsolaoluwa@gmail.com>
2021-04-30 18:25:37 +0100

    Made changes as requested by TSE

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
   
   resources :posts
-  resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
